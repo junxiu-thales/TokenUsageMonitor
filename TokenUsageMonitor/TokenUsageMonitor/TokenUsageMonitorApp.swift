df4699b62663612f38a1178882dc9c89a6d46f8c
Jun Xiu <jun-xiu-j.chan@thalesgroup.com>
2026-03-10 22:05:55 +0800

    init project

---
+++ TokenUsageMonitor/TokenUsageMonitor/TokenUsageMonitorApp.swift
@@ -0,0 +1,17 @@
+//
+//  TokenUsageMonitorApp.swift
+//  TokenUsageMonitor
+//
+//  Created by Chan Jun Xiu on 10/3/26.
+//
+
+import SwiftUI
+
+@main
+struct TokenUsageMonitorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
